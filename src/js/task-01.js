314554581d668caa00d3fc2d209032114b0c7e9c
Volodymyr-Oliynyk <93007917+Volodymyr-Oliynyk@users.noreply.github.com>
2022-05-13 00:11:25 +0300

    task1

---
+++ src/js/task-01.js
@@ -0,0 +1,10 @@
+const itemList = document.querySelectorAll(".item");
+console.log(`Number of categories: ${itemList.length}`);
+
+itemList.forEach(element => { 
+    const titleCategories = element.querySelector("h2");
+    console.log("Category:", titleCategories.innerHTML)
+
+    const elementsCategories = element.querySelectorAll("li")
+    console.log("Elements:", elementsCategories.length)
+});
