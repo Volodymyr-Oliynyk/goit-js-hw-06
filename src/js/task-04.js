b2619eaa486c1c587f7f0ccd0a8006ad674c071a
Volodymyr-Oliynyk <93007917+Volodymyr-Oliynyk@users.noreply.github.com>
2022-05-13 14:51:24 +0300

    4 task

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,15 +1,28 @@
 const value = document.querySelector('#value');
 console.log(value);
+
 let counterValue = 0;
 console.log(counterValue);
-const counterValuePlus = document.querySelector('[data-action="increment"]')
-console.log(counterValuePlus);
+
 const counterValueMinus = document.querySelector('[data-action="decrement"]');
 console.log(counterValueMinus);
 
+const counterValuePlus = document.querySelector('[data-action="increment"]')
+console.log(counterValuePlus);
+
+counterValuePlus.addEventListener('click', () => {
+    counterValue +=1; 
+    updateCounterValue();
+})
 
 counterValueMinus.addEventListener("click", () => {
-    console.log('click')
+    counterValue -=1;
+    updateCounterValue();
+ 
 });
 
-// button.addEventListener("click", handleClick);
+function updateCounterValue(){
+    value.textContent = counterValue;
+}
+
+
