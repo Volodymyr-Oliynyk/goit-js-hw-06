7c803d1c2eac9867b42cac94eb78181e6129b379
Volodymyr-Oliynyk <93007917+Volodymyr-Oliynyk@users.noreply.github.com>
2022-05-13 22:35:39 +0300

    task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector("body");
+const button = document.querySelector(".change-color");
+const span =  document.querySelector(".color");
+
+button.addEventListener('click', onButtonClick);
+
+function onButtonClick(currentColor){
+ currentColor = getRandomHexColor()
+  body.style.backgroundColor = currentColor;
+  span.textContent = currentColor;
+}
+
